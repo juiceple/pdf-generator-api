17dedcdeafb75acbcd1d75542b99aed28ae8a035
Hyeon Jong Kim <khje1027@gmail.com>
2024-11-03 15:22:50 +0900

    파일 제한 늘리기

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const puppeteer = require('puppeteer');
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '10mb' })); // 요청 본문 크기 제한을 10MB로 설정
 
 app.post('/generate-pdf', async (req, res) => {
   const { html, css } = req.body;
